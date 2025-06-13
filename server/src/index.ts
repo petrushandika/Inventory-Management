394175319fe4d96e4031ff359cc7fbc4cf0d6f88
Petrus Handika <petrushandikasinaga@gmail.com>
2025-06-13 10:33:23 +0700

    fix: fix package server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,26 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
+// Configuration
+dotenv.config();
 const app = express();
-const port = 5000;
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get("/hello", (req, res) => {
+  res.send("Hello World");
 });
 
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
