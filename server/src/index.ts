55bfad900ade8967429c17110b01701bf92724ef
Petrus Handika <petrushandikasinaga@gmail.com>
2025-06-17 09:25:32 +0700

    feat: implement users feature

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,7 @@ app.use(cors());
 
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
