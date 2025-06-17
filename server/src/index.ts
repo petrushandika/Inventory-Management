8a8d62ea8b25cc6a77072e426e35e4fbaf503a14
Petrus Handika <petrushandikasinaga@gmail.com>
2025-06-17 11:38:28 +0700

    style: styling expenses component

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
+import expenseRoutes from "./routes/expenseRoutes";
 
 dotenv.config();
 const app = express();
@@ -21,6 +22,7 @@ app.use(cors());
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
+app.use("/expenses", expenseRoutes);
 
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
